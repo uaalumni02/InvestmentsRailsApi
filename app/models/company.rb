0a5e22cd941dde744dc6ea62e7191b2b7b73aec7
Demeco <Demeco_Bell@Demecos-MBP.attlocal.net>
2021-10-17 18:19:30 -0400

    add many to many relationship

--- app/models/company.rb
+++ app/models/company.rb
@@ -1,2 +1,4 @@
 class Company < ApplicationRecord
+    has_many :investments
+    has_many :investors, through: :investments
 end
--- app/models/investor.rb
+++ app/models/investor.rb
@@ -1,2 +1,4 @@
 class Investor < ApplicationRecord
+    has_many :investments 
+    has_many :companies, through: :investments 
 end
